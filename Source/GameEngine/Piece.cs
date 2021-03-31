f0d22464c16d37ddf4089a40aebb4bdb26616cb5
Frej Björnsson <frej_bjornsson@hotmail.com>
2021-03-31 11:11:04 +0200

    piece of cake

---
+++ Source/GameEngine/Piece.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+
+    class Piece
+    {
+        public int PieceID { get; set; }
+        public string Color { get; set; }
+        public int StartingPosition { get; set; } 
+    }
+}

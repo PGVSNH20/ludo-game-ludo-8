93ff06de5e9b0ef1e74c8585f9d6aa1df1556c6a
Frej Björnsson <frej_bjornsson@hotmail.com>
2021-04-05 16:28:58 +0200

    Updated Database and created a DBModel

---
+++ Source/GameEngine/Classes/DBModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace GameEngine
+{
+    class DBModel : DbContext
+    {
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Piece> Pieces { get; set; }
+        public DbSet<Position> Position { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite(@"Data Source=./LudoDB.db");
+        }
+    }
+}
+ 

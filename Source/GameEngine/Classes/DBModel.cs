a7da27b0ba74ce6f6fa3d88c91471799dc921d35
Frej Björnsson <frej_bjornsson@hotmail.com>
2021-04-07 14:33:28 +0200

    commented out DBModel for now

--- Source/GameEngine/Classes/DBModel.cs
+++ Source/GameEngine/Classes/DBModel.cs
@@ -1,23 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿//using Microsoft.EntityFrameworkCore;
 
 
-namespace GameEngine
-{
-    class DBModel : DbContext
-    {
-        public DBModel(): base()
-            {
+//namespace GameEngine
+//{
+//    class DBModel : DbContext
+//    {
+        
 
-            }
+//        public DbSet<Player> Players { get; set; }
+//        public DbSet<Piece> Pieces { get; set; }
+//        public DbSet<Position> Positions { get; set; }
 
-        public DbSet<Player> Players { get; set; }
-        public DbSet<Piece> Pieces { get; set; }
-        public DbSet<Position> Positions { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-        {
-            options.UseSqlite(@"Data Source=./LudoDB.db");
-        }
-    }
-}
+//        protected override void OnConfiguring(DbContextOptionsBuilder options)
+//        {
+//            options.UseSqlite(@"Data Source=./LudoDB.db");
+//        }
+//    }
+//}
  

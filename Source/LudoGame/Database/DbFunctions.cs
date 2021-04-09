7b299b0e5962dd3af509ed0e95ff2d29c532a968
Frej Björnsson <frej_bjornsson@hotmail.com>
2021-04-09 10:35:45 +0200

    added some database stuff

---
+++ Source/LudoGame/Database/DbFunctions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.Database
+{
+    public class DbFunctions
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+
+
+        public void SaveGame(Board BoardID, List<Move> Moves, List<IPlayer> Players, DateTime GameStarted)
+        {
+
+        }
+
+    }
+}
